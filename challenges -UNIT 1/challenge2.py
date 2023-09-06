17c1bd0a3050615a42a418d13377c3459c8f35c1
diya200525 <144214103+diya200525@users.noreply.github.com>
2023-09-05 19:57:59 -1200

    Add files via upload

---
+++ challenges -UNIT 1/challenge2.py
@@ -0,0 +1,15 @@
+"""A program year entered by the user is a leap year or not using ifelif-else statement  """
+
+def isLeapyear(year):
+  if(year % 4== 0 and year % 100 != 0) or year % 400 ==0:
+     return True
+  else:
+    return False
+
+year = int(input(" enter a year :"))
+
+
+if isLeapyear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print(' {} is not a leap year.'.format(year))
